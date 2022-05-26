33e92a58f84ed805c7629e1ddd577a07cf4da4b0
TARIQ ALHARBI 3 <rescco14@gmail.com>
2022-05-26 08:39:01 +0300

    .

---
+++ assets/index.js
@@ -0,0 +1,19 @@
+const txt = document.getElementById('title');
+const prog = 'قلم';
+const bodytxt = document.getElementById('t-body');
+const txtbody = 'منصة تقدم خدمات  متجددة للطلاب ';
+let idx = 1
+let idxx = 1
+setInterval(writeText, 100);
+
+function writeText() {
+    txt.innerText = prog.slice(0, idx);
+    idx++;
+    if (idx > prog.length) {
+        bodytxt.innerHTML = txtbody.slice(0, idxx);
+        idxx++;
+        if (ixx > txtbody) {
+            idxx = 1;
+        }
+    }
+}
