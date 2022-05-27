b7a7681f902cf29f96cb0a332ab472fc05b95b82
TARIQ ALHARBI 3 <rescco14@gmail.com>
2022-05-27 13:06:03 +0300

    .

--- assets/print.js
+++ assets/print.js
@@ -3,13 +3,7 @@
 
 function display(el) {
 
-    var restorpage = document.body.innerHTML;
-
-    var printcontent = document.getElementById(el).innerHTML;
-    document.body.innerHTML = printcontent;
-    document.getElementById('prinText').className += 'text-black';
-
+   
     window.print();
-    document.body.innerHTML = restorpage;
 }
 
